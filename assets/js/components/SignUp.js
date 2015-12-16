bdd08c58481ce8a10c607beefdd37803ed0b0d98
Joe Rehfuss <joe.rehfuss@gmail.com>
2015-12-16 12:47:32 -0800

    signup working

--- assets/js/components/SignUp.js
+++ assets/js/components/SignUp.js
@@ -1,9 +1,54 @@
 var React = require('react');
 var SignUp = React.createClass({
+		getInitialState:function(){
+		return {email:'', password:'', username:''}
+	},
+	afterSignup:function(e){
+		var self = this;
+		e.preventDefault();
+		$.ajax({
+			url: 'http://localhost:3001/api/users',
+			type: 'POST',
+			data: {email:self.state.email, password:self.state.password, username:self.state.username},
+			success: function (data) {
+				console.log(data);
+				self.props.callback();
+			}
+		})
+	},
+	updateEmail:function(e) {
+		this.state.email = e.target.value;
+		this.setState({email:this.state.email})
+	},
+	updatePassword:function(e) {
+		this.state.password = e.target.value;
+		this.setState({password:this.state.password});
+	},
+	updateUsername:function(e) {
+		this.state.username = e.target.value;
+		this.setState({username:this.state.username});
+	},
 	render: function() {
 		return ( 
 			<div >
-				<h1>This will be the Signup page</h1>
+				<form onSubmit={this.afterSignup}>
+					<legend>Form title</legend>
+				
+					<div className="form-group">
+						<label htmlFor="email">email</label>
+						<input type="text" className="form-control" id="email" name='email' placeholder="Input field" onChange={this.updateEmail}/>
+					</div>
+
+					<div className="form-group">
+						<label htmlFor="password">password</label>
+						<input type="password" className="form-control" id="password" name='password' placeholder="Input field" onChange={this.updatePassword}/>
+					</div>
+					<div className="form-group">
+						<label htmlFor="username">username</label>
+						<input type="username" className="form-control" id="username" name='username' placeholder="Input field" onChange={this.updateUsername}/>
+					</div>
+					<button type="submit" className="btn btn-primary">Sign Up!</button>
+				</form>
 			</div>
 		)
 	}
--- models/user.js
+++ models/user.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 var bcrypt   = require('bcrypt');
 
 var UserSchema = mongoose.Schema({
-  username: String,
-  email: String,
-  password: String
+  username:{ type: String, required: true},
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength:8}
 });
 
 UserSchema.set('toJSON', {
