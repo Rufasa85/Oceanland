eea3977266f6b90b4ca32aa6ee565c11ddf9479f
Joe Rehfuss <joe.rehfuss@gmail.com>
2015-12-17 13:51:28 -0800

    updated login/signup styling

--- assets/js/components/Login.js
+++ assets/js/components/Login.js
@@ -28,18 +28,32 @@ var Login = React.createClass({
 		return ( 
 			<div >
 				<form onSubmit={this.afterLogin}>
-					<legend>Form title</legend>
-				
-					<div className="form-group">
-						<label htmlFor="email">email</label>
-						<input type="text" className="form-control" id="email" name='email' placeholder="Input field" onChange={this.updateEmail}/>
+					<div className="row">
+						<div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-md-offset-4">
+							<h1>Login</h1>
+						</div>
 					</div>
-
-					<div className="form-group">
-						<label htmlFor="password">password</label>
-						<input type="password" className="form-control" id="password" name='password' placeholder="Input field" onChange={this.updatePassword}/>
+					<div className="row">
+						<div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-md-offset-4">								
+							<div className="form-group">
+								<label htmlFor="email">Email</label>
+								<input type="text" className="form-control" id="email" name='email' placeholder="ex: joe@joe.joe" onChange={this.updateEmail}/>
+							</div>
+						</div>						
 					</div>
-					<button type="submit" className="btn">Login!</button>
+					<div className="row">
+						<div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-md-offset-4">	
+							<div className="form-group">
+								<label htmlFor="password">password</label>
+								<input type="password" className="form-control" id="password" name='password' placeholder="min. 8 characters" onChange={this.updatePassword}/>
+							</div>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-md-offset-4">
+							<button type="submit" className="btn">Login!</button>
+						</div>
+					</div>
 				</form>
 			</div>
 		)
