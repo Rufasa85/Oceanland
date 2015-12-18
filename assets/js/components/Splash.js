4841a05dadeb90ccb07fc22db9e83c9c2e9d46aa
Joe Rehfuss <joe.rehfuss@gmail.com>
2015-12-17 19:16:04 -0800

    updating copy

--- assets/js/components/Splash.js
+++ assets/js/components/Splash.js
@@ -4,7 +4,7 @@ var Splash = React.createClass({
 		return ( 
 			<div id="fullpage">
 			    <div id='fullpage-first' className="section">
-			    	<h1 className= 'text-center big-header'> Welcome to OceanLand!*</h1>
+			    	<h1 className= 'text-center big-header'> Welcome to OceanLand!</h1>
 			    </div>
 			    <div id='fullpage-second' className="section">
 			    	<h1 className='text-center big-header'>What We Are All About</h1>
@@ -18,7 +18,7 @@ var Splash = React.createClass({
 			    	<h1 className='text-center big-header'>Our App</h1>
 			    	<div className="row">
 			    		<div className="col-xs-10 col-sm-10 col-md-10 col-lg-10 col-xs-offset-1">
-			    			<h3><strong>coming soon</strong></h3>
+			    			<h3><strong>Interested in visiting our park but can't make the trip? Our app is here to save the day! Browse pictures of various animals you can find around the park.  Create an account and you can add animals and pictures of your own. Try it today!</strong></h3>
 			    		</div>
 			    	</div>
 			    </div>
