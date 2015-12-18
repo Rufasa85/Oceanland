8e9cd878c805d60c888d26889bf6de96e67fb5ce
Joe Rehfuss <joe.rehfuss@gmail.com>
2015-12-17 19:37:55 -0800

    about page complete

--- assets/js/components/About.js
+++ assets/js/components/About.js
@@ -3,7 +3,8 @@ var About = React.createClass({
 	render: function() {
 		return ( 
 			<div >
-				<h1>This will be the about page</h1>
+				<h1>About</h1>
+				<h3>OceanLand is a parody SeaWorld app built in ReactJs, NodeJS, Express, and MongoDB by me, Joe Rehfuss, in one week as a class project for a General Assembly Web Development Immersive class.  Read more about OceanLand on the <strong><a href="https://github.com/Rufasa85/Oceanland" target='_blank'>GitHub page</a></strong> for this project.</h3>
 			</div>
 		)
 	}
