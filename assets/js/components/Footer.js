d430350137ddabf3387a6d424c55c932b8b69223
Joe Rehfuss <joe.rehfuss@gmail.com>
2015-12-17 20:09:54 -0800

    footer completed

--- assets/js/components/Footer.js
+++ assets/js/components/Footer.js
@@ -3,7 +3,7 @@ var Footer = React.createClass({
 	render: function() {
 		return ( 
 			<div className='footer'>
-				<p> &copy;2015 Joe Rehfuss </p>
+				<h4 className='text-center'> &copy;2015 Joe Rehfuss <span><a href="https://www.linkedin.com/in/joerehfuss"><i className="fa fa-linkedin-square" target='_blank'></i></a></span><span><a href="https://github.com/Rufasa85" target='_blank'><i className = 'fa fa-github'></i></a></span></h4>
 			</div>
 		)
 	}
